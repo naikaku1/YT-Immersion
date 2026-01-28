598963f869b039745a8ff77b405148982fd23657
Naikaku <naikaku00@gmail.com>
2026-01-28 18:58:01 +0900

    Update page_script.js

--- page_script.js
+++ page_script.js
@@ -5,13 +5,13 @@
 
     try {
         const player = document.getElementById('movie_player');
-        
+
         const tracklist = player ? player.getOption('captions', 'tracklist') : [];
-        
+
         const activeTrack = player ? player.getOption('captions', 'track') : null;
-        
-        window.dispatchEvent(new CustomEvent(eventId, { 
-            detail: { list: tracklist, active: activeTrack } 
+
+        window.dispatchEvent(new CustomEvent(eventId, {
+            detail: { list: tracklist, active: activeTrack }
         }));
     } catch(e) {
         window.dispatchEvent(new CustomEvent(eventId, { detail: { list: [], active: null } }));
